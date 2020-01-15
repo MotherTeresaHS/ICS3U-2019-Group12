cb4fa051b5744e2c104e9d3588a434b98bcdaffe
andrei-chirilov <andrei.chirilov@mths.ca>
2020-01-15 19:43:32 +0000

    Program Add

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_WjjV6r3qZ', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: 'jqb917989n968mp9333rgqp4j' //is this used anywhere?
+    },
+};
